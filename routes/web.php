1d668f006810177d5177c775e341cac92ba6a7f3
Ndiaye Seydou <yonguibrazz@gmail.com>
2025-03-04 13:11:54 +0100

    inacheve fin

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// routes/web.php
 Route::get('/', function () {
-    return view('welcome');
+    return view('template');
 });
+
+
+
